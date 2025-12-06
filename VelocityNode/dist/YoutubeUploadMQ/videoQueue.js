3b3313c5e4d2b4017614742acd4dffe70e6ef175
Aleksa404 <aleksantic1@gmail.com>
2025-12-06 05:16:16 +0100

    removed dist

--- VelocityNode/dist/YoutubeUploadMQ/videoQueue.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.videoUploadQueue = void 0;
-const bullmq_1 = require("bullmq");
-const Redis_1 = require("./Redis");
-exports.videoUploadQueue = new bullmq_1.Queue("video-upload-queue", {
-    connection: Redis_1.redisClient,
-});
-//# sourceMappingURL=videoQueue.js.map
